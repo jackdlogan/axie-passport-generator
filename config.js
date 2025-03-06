c15d5ab06c18e04201c88d43164243ff496da3a8
jackdlogan <nhutphuong.diep@gmail.com>
2025-03-06 17:52:51 +0700

    Fixing internal issue

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_KEY: process.env.AXIE_API_KEY || 'mhkLTvd8ayQ8arSANYK48sbJ6kmf95o9' // fallback for development
+    API_KEY: process.env.AXIE_API_KEY  // fallback for development
 };
 
 export default config; 
